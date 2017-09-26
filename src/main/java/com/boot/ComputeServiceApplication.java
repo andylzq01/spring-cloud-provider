5738a2eab965376487890b1bfd4c33845ae9bd66
laizeqi <lai.zq@wonhigh.com>
2017-09-26 14:31:24 +0800

    '服务提供者'

---
+++ src/main/java/com/boot/ComputeServiceApplication.java
@@ -0,0 +1,16 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+/**
+ * 服务提供者
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ComputeServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ComputeServiceApplication.class, args);
+	}
+}
